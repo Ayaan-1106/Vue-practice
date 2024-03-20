5cecfc6bf8fd458e910767fd0145f852560cb6ef
SeongHyeon Cho <00seong116@naver.com>
2024-03-21 01:13:59 +0900

    second commit

---
+++ src/router/index.js
@@ -0,0 +1,48 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import DataBinding from '../views/DataBinding.vue'
+import MyTest from '../views/MyTest.vue'
+import DataBindingList2 from '../views/DataBindingList2.vue'
+import Nested from '../views/NestedComponent.vue'
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/databinding',
+    name: 'DataBinding',
+    component: DataBinding
+  },
+  {
+    path: '/Mytest',
+    name: 'test',
+    component: MyTest
+  },
+  {
+    path: '/databindinglist2',
+    name: 'DataBindingList2',
+    component: DataBindingList2
+  },
+  {
+    path: '/nested',
+    name: 'NestedComponent',
+    component: Nested
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
